081a67c01821918eac4896d4b5849ea1e88a62f4
Jami <jamsheduddin03@gmail.com>
2023-08-07 23:46:29 +0600

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+var jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
@@ -9,6 +10,27 @@ const port = process.env.PORT || 3000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+
+    req.decoded = decoded;
+    next();
+  });
+};
 
 // database
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -26,7 +48,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const userCollection = client.db("melodyManorDB").collection("users");
     const classCollection = client.db("melodyManorDB").collection("classes");
@@ -36,6 +58,15 @@ async function run() {
     const selectedClassCollection = client
       .db("melodyManorDB")
       .collection("selectedClass");
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res.send({ token });
+    });
 
     //users APIs
 
@@ -205,7 +236,7 @@ async function run() {
       const existInEnrolledClass = userSpecificEnrolledClasses.find(
         (singleClass) => singleClass.classId === selectedClassItem.classId
       );
-      console.log(existInEnrolledClass);
+
       if (existInEnrolledClass) {
         return res.send({ message: "already enrolled" });
       }
